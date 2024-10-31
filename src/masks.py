bcd17b93997ab8bb13a24d6195b7201593d212d7
Федя Зверев <zverev.fedya@list.ru>
2024-10-31 18:41:34 +0300

    фиксация прогресса по прошлой домашке

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+def get_mask_card_number(card_number: Any = str) -> str:
+    """Функция принимает на входе номер карты и возвращает маску"""
+    if len(card_number) != 16:
+        return "Вы ввели неккоректное значение"
+    else:
+        masked_card_number = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        return masked_card_number
+
+
+def get_mask_account(account_number: Any = str) -> str:
+    """Функия принимает на вход номер счета и возвращает маску"""
+    if len(account_number) != 20:
+        return "Вы ввели неккоректное значение"
+    else:
+        masked_account_number = f"**{account_number[-4:]}"
+        return masked_account_number
