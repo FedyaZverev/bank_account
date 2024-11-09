8948b5973b241bee0f09c88dc016b4599d8ace08
Федя Зверев <zverev.fedya@list.ru>
2024-11-09 17:34:58 +0300

    Initial commit

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(word: str) -> str:
+    """Функция маскирует номер карты/счета"""
+    word_digit = ""
+    word_alpha = ""
+    count = 0
+    for name in word:
+        if name.isdigit():
+            word_digit += name
+            count += 1
+        if name.isalpha():
+            word_alpha += name
+    if count == 16:
+        return f"{word_alpha} {get_mask_card_number(word_digit)}"
+    if count == 20:
+        return f"{word_alpha} {get_mask_account(word_digit)}"
+    return "Вы ввели неверный формат"
+
+
+def get_date(time: str) -> str:
+    """Функция принимает строку и возвращает значение в формате ДД.ММ.ГГГГ"""
+    return f"{time[8:10]}.{time[5:7]}.{time[0:4]}"
