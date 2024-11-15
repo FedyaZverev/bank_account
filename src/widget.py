5637585dd37aee81d67502eeadfae1b76016c7d0
Федя Зверев <zverev.fedya@list.ru>
2024-11-15 07:13:15 +0300

    refactor code

--- src/widget.py
+++ src/widget.py
@@ -3,17 +3,17 @@ from typing import Any
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(word: Any = str) -> str:
+def mask_account_card(mask_number: Any = str) -> str:
     """Функция маскирует номер карты/счета"""
     word_digit = ""
     word_alpha = ""
     count = 0
-    for name in word:
-        if name.isdigit():
-            word_digit += name
+    for char in mask_number:
+        if char.isdigit():
+            word_digit += char
             count += 1
-        if name.isalpha():
-            word_alpha += name
+        if char.isalpha():
+            word_alpha += char
     if count == 16:
         return f"{word_alpha} {get_mask_card_number(word_digit)}"
     if count == 20:
@@ -21,6 +21,6 @@ def mask_account_card(word: Any = str) -> str:
     return "Вы ввели неверный формат"
 
 
-def get_date(time: Any = str) -> str:
+def get_date(data_str: Any = str) -> str:
     """Функция принимает строку и возвращает значение в формате ДД.ММ.ГГГГ"""
-    return f"{time[8:10]}.{time[5:7]}.{time[0:4]}"
+    return f"{data_str[8:10]}.{data_str[5:7]}.{data_str[0:4]}"
